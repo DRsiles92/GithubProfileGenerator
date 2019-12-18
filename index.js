67ec0b1180ee53ad5eb657a9723b5f9ee9e07c49
Debbie S <dreategu@gmail.com>
2019-12-17 21:15:15 -0500

    Added convert to PDF functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const util = require("util");
-var pdf = require("pdf-creator-node");
+var pdf = require('html-pdf');
 
 const readFileAsync = util.promisify(fs.readFile);
 
@@ -21,58 +21,72 @@ inquirer
     "Pink",
     "Red"
   ]}])
-  .then(function({username}) {
+  .then(function({username, colorChoices}) {
     const queryUrl = `https://api.github.com/users/${username}`;
-
+    
+    console.log(colorChoices);
     axios.get(queryUrl).then(function(response) {
       // User name
       const name = response.data.name;
       console.log(name);
-      fs.appendFileSync('message.pdf', name + "\n" + response.data.avatar_url + "\n" + response.data.html_url + "\n" + response.data.location + "\n" + response.data.bio + "\n" + response.data.public_repos + "\n" + response.data.followers + "\n" + response.data.following);
-      console.log(response.data.name);
-      // Profile image
-      // fs.appendFileSync('message.pdf', response.data.avatar_url);
-        console.log(response.data.avatar_url);
-        // Link to User GitHub profile
-        console.log(response.data.html_url);
-        // location
-        console.log(response.data.location);
-        // User bio
-        console.log(response.data.bio);
-        // Number of public repositories
-        console.log(response.data.public_repos);
-        // Number of followers
-        console.log(response.data.followers);
-        // Number of users following
-        console.log(response.data.following);
+      fs.writeFile('message.html', `<!DOCTYPE html>
+      <html>
+          <head>
+              <mate charest="utf-8" />
+              <title>Github Profile</title>
+          </head>
+          <body>
+              <h1> <span style="color:`+colorChoices+`">` + name + `</span></h1>
+              <hr>
+              <img src="`+response.data.avatar_url+`" alt="profilepic">
+              <br>
+              <P>Bio: ` + response.data.bio + `
+              <br>
+              Company: ` + response.data.company + `
+              <br>
+              Repo URL: <a href="`+response.data.html_url+`">`+username+`</a>
+              <br>
+              Public Repos:`+ response.data.public_repos +` 
+              <br>
+              Followers: `+ response.data.followers +`
+              <br>
+              Following: `+ response.data.following +`
+              <br>
+              Location: `+ response.data.location +`
+              </p>
+          </body>
+      </html>`
+      ,
+      function (err) {
+        if (err) throw err;
+        console.log('Saved!');
       });
-
-
-
-      });
-
-// function generateHTML(answers) {
-// return `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//   <title>Document</title>
-// </head>
-// <body>
-//   <div class="jumbotron jumbotron-fluid">
-//   <div class="container">
-//     <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-//     <p class="lead">I am from ${answers.location}.</p>
-//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//     <ul class="list-group">
-//       <li class="list-group-item">My GitHub username is ${answers.github}</li>
-//       <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-//     </ul>
-//   </div>
-// </div>
-// </body>
-// </html>`;
-// }
+      // // );
+      // console.log(response.data.name);
+      // // Profile image
+      // // fs.appendFileSync('message.pdf', response.data.avatar_url);
+      //   console.log(response.data.avatar_url);
+      //   // Link to User GitHub profile
+      //   console.log(response.data.html_url);
+      //   // location
+      //   console.log(response.data.location);
+      //   // User bio
+      //   console.log(response.data.bio);
+      //   // Number of public repositories
+      //   console.log(response.data.public_repos);
+      //   // Number of followers
+      //   console.log(response.data.followers);
+      //   // Number of users following
+      //   console.log(response.data.following);
+      
+    });
+    
+  });
+  var html = fs.readFileSync('./message.html', 'utf8');
+  var options = { format: 'Letter' };
+  
+  pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+    if (err) return console.log(err);
+    console.log(res); // { filename: '/app/businesscard.pdf' }
+  });
+    
